75ee9a965080ad45ea3dcc8c630493a8fae08030
GangbacOl <gangyeonjo336@gmail.com>
2021-01-05 01:56:26 +0900

    ADD: create graphql server

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './graphql/resolver';
+
+const server = new GraphQLServer({ typeDefs: 'graphql/schema.graphql', resolvers });
+
+server.start(() => console.log('GraphQL Server Running'));
